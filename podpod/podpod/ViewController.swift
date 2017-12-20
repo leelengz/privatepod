89e4d7aa01d898432fa5a66c3e67a0fdc2cf4e08
Kornthip Nakchamratsri <kornthip@23perspective.com>
2017-12-20 11:45:33 +0700

    init sourcecode

---
+++ podpod/podpod/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  podpod
+//
+//  Created by Kornthip Nakchamratsri on 12/20/2560 BE.
+//  Copyright © 2560 Kornthip Nakchamratsri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
